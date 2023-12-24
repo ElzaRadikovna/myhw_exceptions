194bed781810ff34d1f790f9fe9d78d36e4d3ce4
Neto test <akmalova.elza@yandex.ru>
2023-12-24 15:04:43 +0300

    Second commit

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    @Test
+    public void testRemoveWhenProductExist() {
+
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Книга", 300);
+        Product product2 = new Product(2, "Журнал", 200);
+        Product product3 = new Product(3, "Блокнот", 100);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(2);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemoveWhenProductNotExist() {
+
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Книга", 300);
+        Product product2 = new Product(2, "Журнал", 200);
+        Product product3 = new Product(3, "Блокнот", 100);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertThrows(NotFoundException.class, () -> repo.remove(200));
+
+
+    }
+
+
+}
